8b4d94fff967279a7422896c961356a65afb31a7
Sleon4 <sergioleon4004@hotmail.com>
2025-03-19 10:51:49 -0500

    feat: dashboard module is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import "./assets/index.css";
 
-import { Fragment } from "react";
-import { Container } from "react-bootstrap";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dasboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Fragment>
-              <Container>
-                <h1>Hola</h1>
-              </Container>
-            </Fragment>
-          }
-        />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );

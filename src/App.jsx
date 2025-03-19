fed10077295f552c1c2aff774f3265e01b869059
Sleon4 <sergioleon4004@hotmail.com>
2025-03-19 10:48:18 -0500

    build: dependencies are installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import { Fragment } from 'react'
+import "./assets/index.css";
+
+import { Fragment } from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Fragment></Fragment>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Fragment>
+              <Container>
+                <h1>Hola</h1>
+              </Container>
+            </Fragment>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

556009499dbeb3e6d7d62d4c87a310391ae35a2b
Sleon4 <sergioleon4004@hotmail.com>
2025-03-20 12:04:10 -0500

    feat: Footer is added

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,34 @@ import Boxes from "./pages/boxes/Boxes";
 import Login from "./pages/auth/login/Login";
 import Register from "./pages/auth/register/Register";
 import Users from "./pages/users/Users";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <div className="d-flex flex-column min-vh-100">
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <main class="flex-grow-1 container py-4">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
 
-        <Route path="auth">
-          <Route path="login" element={<Login />} />
+            <Route path="auth">
+              <Route path="login" element={<Login />} />
 
-          <Route path="register" element={<Register />} />
-        </Route>
+              <Route path="register" element={<Register />} />
+            </Route>
 
-        <Route path="boxes" element={<Boxes />} />
+            <Route path="boxes" element={<Boxes />} />
 
-        <Route path="suppliers" element={<Suppliers />} />
+            <Route path="suppliers" element={<Suppliers />} />
 
-        <Route path="users" element={<Users />} />
-      </Routes>
+            <Route path="users" element={<Users />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
